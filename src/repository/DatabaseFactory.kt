7ebf20e9d68ceece2576d73c7e3e6761564b17ce
Suraj <surajsinhrathod75@gmail.com>
2022-05-10 14:47:20 +0530

    procfile added

--- src/repository/DatabaseFactory.kt
+++ src/repository/DatabaseFactory.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
 
 object DatabaseFactory {
 
@@ -30,10 +31,18 @@ object DatabaseFactory {
         val config = HikariConfig()
 
         config.driverClassName = "org.postgresql.Driver"
-        config.jdbcUrl = "jdbc:postgresql:ProgramDatabase?user=postgres&password=787250"
+        //config.jdbcUrl = "jdbc:postgresql:ProgramDatabase?user=postgres&password=787250"
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+
+
+        val uri = URI(System.getenv("DATABASE_URL"))
+        val username = uri.userInfo.split(":").toTypedArray()[0]
+        val password = uri.userInfo.split(":").toTypedArray()[1]
+
+        config.jdbcUrl =
+            "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
         config.validate()
 
         return HikariDataSource(config)
