e30c7a05a685af4d7b7e8e39c25b78ceaad64459
Suraj <surajsinhrathod75@gmail.com>
2022-07-28 22:21:03 +0530

    Revert "hii"
    This reverts commit ee04945f

--- src/data/model/Program.kt
+++ src/data/model/Program.kt
@@ -3,7 +3,6 @@ package com.example.data.model
 import java.util.*
 
 data class Program(
-
     val id : Int = Random().nextInt(),
     val title : String,
     val content : String,
@@ -11,14 +10,3 @@ data class Program(
     val sub : String,
     val unit : String
 )
-
-
-/*
-val id : Int = Random().nextInt(),
-    val title : String,
-    val content : String,
-    val sem : String,
-    val sub : String,
-    val unit : String
-)
-*/
